c23f96a96ead7f1bebdfdd9e7174f1f9a7c696b7
semplicio <60362115+semplicio@users.noreply.github.com>
2026-08-13 08:24:30 -0300

    Bump Network Probe to v1.3.0

--- networkprobe/build.gradle.kts
+++ networkprobe/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.autombot.networkprobe"
         minSdk = 26
         targetSdk = 34
-        versionCode = 12
-        versionName = "1.2.0"
+        versionCode = 13
+        versionName = "1.3.0"
     }
 
     buildTypes {
